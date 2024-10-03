0e1755b8f5c64c2a4d5fb22bce398fe3247b4a88
Nekraket _ <ekaterinanekrasova89798@gmail.com>
2024-10-03 09:36:13 +0700

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GraphicEditor
+{
+    public partial class MainWindow : Window
+    {
+        private double brushSize = 5;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            IfCancasNeNull();
+        }
+
+        private void BrushSizeSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            brushSize = e.NewValue;
+            IfCancasNeNull();
+        }
+
+        private void lbColors_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            IfCancasNeNull();
+        }
+
+
+
+        private void IfCancasNeNull()
+        {
+            if (Canvas != null)
+            {
+                Canvas.DefaultDrawingAttributes.Width = brushSize;
+                Canvas.DefaultDrawingAttributes.Height = brushSize;
+            }
+        }
+
+    }
+}
